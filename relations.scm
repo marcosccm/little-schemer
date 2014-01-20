d4a203cc402ad4487da31ee685018cd73e2f461c
Marcos Matos <marcosccm@gmail.com>
2014-01-20 10:46:56 -0200

    Moar seeets

--- relations.scm
+++ relations.scm
@@ -20,3 +20,36 @@
       ((null? s1) #t)
       ((member? (car s1) s2) (subset? (cdr s1) s1))
       (else #f))))
+
+(define eqset?
+  (lambda (s1 s2)
+    (and
+      (subset? s1 s2)
+      (subset? s2 s1))))
+
+(define intersect?
+  (lambda (s1 s2)
+    (cond
+      ((null? s1) #f)
+      ((member? (car s1) s2) #t)
+      (else (intersect? (cdr s1) s2)))))
+
+(define intersect
+  (lambda (s1 s2)
+    (cond
+      ((null? s1) '())
+      ((member? (car s1) s2) (cons (car s1) (intersect (cdr s1) s2)))
+      (else (intersect (cdr s1) s2)))))
+
+(define union
+  (lambda (s1 s2)
+    (cond
+      ((null? s1) s2)
+      ((member? (car s1) s2) (union (cdr s1) s2))
+      (else (cons (car s1) (union (cdr s1) s2))))))
+
+(define intersectall
+  (lambda (lset)
+    (cond
+      ((null? (car lset)) (cdr lset))
+      (else (intersect (car lset) (intersectall (cdr lset)))))))
