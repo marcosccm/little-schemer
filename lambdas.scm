fc777f260f5d0e74804b348bdaca5208808aa31b
Marcos Matos <marcosccm@gmail.com>
2014-01-20 11:33:47 -0200

    Refactor to lambdas

--- lambdas.scm
+++ lambdas.scm
@@ -1,18 +1,20 @@
-(define insertR-f
-  (lambda (test?)
-    (lambda (new old l)
-      (cond
-        ((null? l) '())
-        ((test? (car l) old)
-         (cons old (cons new (cdr l))))
-        (else (cons (car l) 
-                ((insertR-f test?) new old (cdr l))))))))
 
-(define insertL-f
-  (lambda (test?)
+(define insert-g
+  (lambda (seq)
     (lambda (new old l)
       (cond
         ((null? l) '())
-        ((test? (car l) old) (cons new l))
+        ((eq? (car l) old) (seq new old l))
         (else (cons (car l)
-                ((insertL-f test?) new old (cdr l))))))))
+                ((insert-g seq) new old (cdr l))))))))
+
+(define seqL
+  (lambda (new old l)
+    (cons new (cons old l))))
+
+(define seqR
+  (lambda (new old l)
+    (cons old (cons new l))))
+
+(define insertL (insert-g seqL))
+(define insertR (insert-g seqR))
