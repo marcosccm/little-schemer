7d33403d72a2355fb4e95df19062e2f6b307d49b
Marcos Matos <marcosccm@gmail.com>
2014-01-20 18:08:58 -0200

    Lambdas! Lambdas!

--- lambdas.scm
+++ lambdas.scm
@@ -1,12 +1,31 @@
+(define eq?-c
+  (lambda (x)
+    (lambda (y)
+      (eq? x y))))
+
+(define eq-salad? (eq?-c 'salad))
+
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+        ((null? l) '())
+        ((test? (car l) a) (cdr l))
+        (else (cons (car l) ((rember-f test?) a (cdr l))))))))
+
+(define rember-eq (rember-f eq?))
 
 (define insert-g
   (lambda (seq)
     (lambda (new old l)
       (cond
         ((null? l) '())
-        ((eq? (car l) old) (seq new old l))
-        (else (cons (car l)
-                ((insert-g seq) new old (cdr l))))))))
+        ((eq? (car l) old)
+          (seq new old (cdr l)))
+        (else
+          (cons
+            (car l)
+            ((insert-g seq) new old (cdr l))))))))
 
 (define insertL
   (insert-g
@@ -17,6 +36,35 @@
   (insert-g
     (lambda (new old l)
       (cons old (cons new l)))))
+
+(define subst
+  (insert-g
+    (lambda (new old l)
+      (cons new l))))
+
+(define yyy
+  (lambda (a l)
+    ((insert-g seqrem) #f a l)))
+
+(define seqrem
+  (lambda (new old l)
+    l))
+
+(define atom-to-function
+  (lambda (x)
+    (cond
+      ((eq? x '+) add)
+      ((eq? x 'x) mult)
+      (else expo))))
+
+(define value
+  (lambda (aexp)
+    (cond
+      ((atom? aexp) aexp)
+      (else
+        ((atom-to-function (operator aexp))
+          (value (fst-sub-exp aexp))
+          (value (snd-sub-exp aexp)))))))
 
 (define even?
   (lambda (x)
