666d826a87fac8cb2e32ac460ca20894bef0787e
Marcos Matos <marcosccm@gmail.com>
2014-01-21 18:29:34 -0200

    Understanding collectors

--- lambdas.scm
+++ lambdas.scm
@@ -1,3 +1,5 @@
+(load "numbers.scm")
+
 (define eq?-c
   (lambda (x)
     (lambda (y)
@@ -82,6 +84,70 @@
         ((test? (car lat) a) ((multirember-f test?) a (cdr lat)))
         (else (cons (car lat)
                     ((multirember-f test?) a (cdr lat))))))))
+
+(define multiremberT
+  (lambda (test? lat)
+    (cond
+      ((null? lat) '())
+      ((test? (car lat)) (multiremberT test? (cdr lat)))
+      (else cons (car lat)
+            (multiremberT test? (cdr lat))))))
+
+(define multirember-co
+  (lambda (a lat col)
+    (cond
+      ((null? lat) (col '() '()))
+      ((eq? (car lat) a)
+        (multirember-co a
+          (cdr lat)
+          (lambda (newlat seen)
+            (col newlat (cons (car lat) seen)))))
+      (else
+        (multiremner-co a
+          (cdr lat)
+          (lambda (newlat seen)
+            (col (cons (car lat) newlat) seen)))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old)
+       (cons new (cons old (multiinsertL new old (cdr l)))))
+      (else
+        (cons (car l) (multiinsertL new old (cdr l)))))))
+
+(define multiinsertLR
+  (lambda (new oldL oldR lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) oldL)
+       (cons new (cons oldL (multiinsertLR oldL oldR (cdr lat)))))
+      ((eq? (car lat) oldR)
+       (cons oldR (cons new (multiinsertLR oldL oldR (cdr lat)))))
+      (else
+        (cons (car lat) (multiinsert oldL oldR (cdr lat)))))))
+
+(define multiinsertLR-co
+  (lambda (new oldL oldR lat col)
+    (cond
+      ((null? lat) (col '() 0 0))
+      ((eq? (car lat) oldL)
+       (multiinsertLR-co new oldL oldR
+        (cdr lat)
+        (lambda (newlat l r)
+          (col (cons new (cons oldL newlat)) (add1 l) r))))
+      ((eq? (car lat) oldR)
+       (multiinsertLR-co new oldL oldR
+        (cdr lat)
+        (lambda (newlat l r)
+          (col (cons oldR (cons new newlat)) l (add1 r)))))
+      (else
+        (multiinsertLR-co new oldL oldR
+          (cdr lat)
+          (lambda (newllat l r)
+            (cons (car lat) newlat) l r))))))
+
 
 (define even?
   (lambda (x)
